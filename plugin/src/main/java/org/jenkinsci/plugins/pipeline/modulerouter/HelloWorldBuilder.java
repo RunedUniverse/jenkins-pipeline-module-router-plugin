2d814f3b79f2e81fb1835ccb4dbba8d5fcfb4c8f
VenaNocta <venanocta@gmail.com>
2024-04-29 15:17:14 +0200

    readded plugin, targeting scripted pipelines

---
+++ plugin/src/main/java/org/jenkinsci/plugins/pipeline/modulerouter/HelloWorldBuilder.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright © 2024 VenaNocta (venanocta@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jenkinsci.plugins.pipeline.modulerouter;
+
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import hudson.util.FormValidation;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import jenkins.tasks.SimpleBuildStep;
+import org.jenkinsci.Symbol;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+
+public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
+
+	private final String name;
+	private boolean useFrench;
+
+	@DataBoundConstructor
+	public HelloWorldBuilder(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isUseFrench() {
+		return useFrench;
+	}
+
+	@DataBoundSetter
+	public void setUseFrench(boolean useFrench) {
+		this.useFrench = useFrench;
+	}
+
+	@Override
+	public void perform(Run<?, ?> run, FilePath workspace, EnvVars env, Launcher launcher, TaskListener listener)
+			throws InterruptedException, IOException {
+		if (useFrench) {
+			listener.getLogger()
+					.println("Bonjour, " + name + "!");
+		} else {
+			listener.getLogger()
+					.println("Hello, " + name + "!");
+		}
+	}
+
+	@Symbol("greet")
+	@Extension
+	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+		public FormValidation doCheckName(@QueryParameter String value, @QueryParameter boolean useFrench)
+				throws IOException, ServletException {
+			if (value.length() == 0)
+				return FormValidation.error(Messages.HelloWorldBuilder_DescriptorImpl_errors_missingName());
+			if (value.length() < 4)
+				return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_tooShort());
+			if (!useFrench && value.matches(".*[éáàç].*")) {
+				return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench());
+			}
+			return FormValidation.ok();
+		}
+
+		@Override
+		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+			return true;
+		}
+
+		@Override
+		public String getDisplayName() {
+			return Messages.HelloWorldBuilder_DescriptorImpl_DisplayName();
+		}
+	}
+}
